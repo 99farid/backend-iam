df7ad91e7911e7e8babc1d781cb4b393ba37aa03
savitaarsarini <desakayuputu5@gmail.com>
2021-12-13 09:34:15 +0700

    tambah anotasi mainclass

--- assets-management/src/main/java/com/lawencon/assetsmanagement/AssetsManagementMainApp.java
+++ assets-management/src/main/java/com/lawencon/assetsmanagement/AssetsManagementMainApp.java
@@ -1,7 +1,11 @@
 package com.lawencon.assetsmanagement;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class AssetsManagementMainApp {
 	public static void main(String[] args) {
-		
+		SpringApplication.run(AssetsManagementMainApp.class, args);
 	}
 }
