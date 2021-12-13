696b1133b8965aa555cc00c8dfc74258409715d8
savitaarsarini <desakayuputu5@gmail.com>
2021-12-13 13:37:54 +0700

    isi kelas transactionOut

--- assets-management/src/main/java/com/lawencon/assetsmanagement/model/DetailTransactionOut.java
+++ assets-management/src/main/java/com/lawencon/assetsmanagement/model/DetailTransactionOut.java
@@ -1,5 +1,51 @@
 package com.lawencon.assetsmanagement.model;
 
-public class DetailTransactionOut {
+import java.time.LocalDate;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.Table;
+
+import com.lawencon.base.BaseEntity;
+
+@Entity
+@Table(name = "detail_transaction_out")
+public class DetailTransactionOut extends BaseEntity{
+
+	private static final long serialVersionUID = 5738129835522199612L;
+
+	@JoinColumn(name = "id_transaction_out", nullable = false)
+	private TransactionOut idTransactionOut;
+	
+	@JoinColumn(name = "id_asset", nullable = false)
+	private Asset idAsset;
+
+	@Column(name = "check_out_date", nullable = false)
+	private LocalDate checkOutDate;
+	
+	
+	public TransactionOut setIdTransactionOut() {
+		return idTransactionOut;
+	}
+	
+	public void getIdTransactionOut(TransactionOut idTransactionOut) {
+		this.idTransactionOut = idTransactionOut;
+	}
+	
+	public Asset setIdAsset() {
+		return idAsset;
+	}
+	
+	public void getIdAsset(Asset idAsset) {
+		this.idAsset = idAsset;
+	}
+	
+	public LocalDate setCheckOutDate() {
+		return checkOutDate;
+	}
+	
+	public void getCheckOutDate(LocalDate checkOutDate) {
+		this.checkOutDate = checkOutDate;
+	}
 }
--- assets-management/src/main/java/com/lawencon/assetsmanagement/model/TransactionOut.java
+++ assets-management/src/main/java/com/lawencon/assetsmanagement/model/TransactionOut.java
@@ -1,5 +1,63 @@
 package com.lawencon.assetsmanagement.model;
 
-public class TransactionOut {
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
 
+import com.lawencon.base.BaseEntity;
+
+@Entity
+@Table(name = "transaction_out")
+public class TransactionOut extends BaseEntity {
+
+	private static final long serialVersionUID = 1259288568796069056L;
+	
+	@Column(length = 32, unique = true, nullable = false)
+	private String code;
+	
+	@ManyToOne
+	@JoinColumn(name = "id_employee", nullable = false)
+	private Employee idEmployee;
+	
+	@ManyToOne
+	@JoinColumn(name = "id_location", nullable = false)
+	private Location idLocation;
+	
+	@ManyToOne
+	@JoinColumn(name = "id_general_item", nullable = false)
+	private Asset idGeneralItem;
+	
+	public String setCode() {
+		return code;
+	}
+	
+	public void getCode(String code) {
+		this.code = code;	
+	}
+	
+	public Employee setIdEmployee() {
+		return idEmployee;
+	}
+	
+	public void getIdEmployee(Employee idEmployee) {
+		this.idEmployee = idEmployee;
+	}
+	
+	public Location setIdLocation() {
+		return idLocation;
+	}
+	
+	public void getIdLocation(Location idLocation) {
+		this.idLocation = idLocation;
+	}
+	
+	public Asset setIdGeneralItem() {
+		return idGeneralItem;
+	}
+	
+	public void getIdGeneralItem(Asset idGeneralItem) {
+		this.idGeneralItem = idGeneralItem;
+	}
 }
