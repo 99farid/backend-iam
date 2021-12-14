edc3a7b07fefab98c864974d3e0b10cc98a51138
savitaarsarini <desakayuputu5@gmail.com>
2021-12-14 18:15:24 +0700

    perbaikan dao impl rolepermission

--- assets-management/src/main/java/com/lawencon/assetsmanagement/dao/impl/RolePermissionsDaoImpl.java
+++ assets-management/src/main/java/com/lawencon/assetsmanagement/dao/impl/RolePermissionsDaoImpl.java
@@ -4,25 +4,25 @@ import java.util.List;
 
 import org.springframework.stereotype.Repository;
 
-import com.lawencon.assetsmanagement.dao.PermissionsDetailDao;
-import com.lawencon.assetsmanagement.model.PermissionsDetail;
+import com.lawencon.assetsmanagement.dao.RolePermissionsDao;
+import com.lawencon.assetsmanagement.model.RolePermissions;
 import com.lawencon.base.BaseDaoImpl;
 
 @Repository
-public class PermissionsDetailDaoImpl extends BaseDaoImpl<PermissionsDetail> implements PermissionsDetailDao {
+public class RolePermissionsDaoImpl extends BaseDaoImpl<RolePermissions> implements RolePermissionsDao {
 	
 	@Override
-	public List<PermissionsDetail> findAll() throws Exception {
+	public List<RolePermissions> findAll() throws Exception {
 		return getAll();
 	}
 	
 	@Override
-	public PermissionsDetail findById(String id) throws Exception {
+	public RolePermissions findById(String id) throws Exception {
 		return getById(id);
 	}
 	
 	@Override
-	public PermissionsDetail saveOrUpdate(PermissionsDetail data) throws Exception {
+	public RolePermissions saveOrUpdate(RolePermissions data) throws Exception {
 		return save(data);
 	}
 	
