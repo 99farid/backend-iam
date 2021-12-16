831422ee0b75ca7d29f2123e04c91233c0e3d3f6
savitaarsarini <desakayuputu5@gmail.com>
2021-12-16 13:33:16 +0700

    perbaikan nama respon di service detail transaksi

--- assets-management/src/main/java/com/lawencon/assetsmanagement/service/DetailTransactionsOutService.java
+++ assets-management/src/main/java/com/lawencon/assetsmanagement/service/DetailTransactionsOutService.java
@@ -1,14 +1,14 @@
 package com.lawencon.assetsmanagement.service;
 
-import com.lawencon.assetsmanagement.dto.detailtransactionsout.FindAllDetailTransactionsOutDto;
-import com.lawencon.assetsmanagement.dto.detailtransactionsout.FindByIdDetailTransactionsOutDto;
+import com.lawencon.assetsmanagement.dto.detailtransactionsout.FindAllResDetailTransactionsOutDto;
+import com.lawencon.assetsmanagement.dto.detailtransactionsout.FindByIdResDetailTransactionsOutDto;
 import com.lawencon.assetsmanagement.dto.detailtransactionsout.FindByIdResHeaderDto;
 
 public interface DetailTransactionsOutService {
 
-	FindAllDetailTransactionsOutDto findAll() throws Exception;
+	FindAllResDetailTransactionsOutDto findAll() throws Exception;
 	
-	FindByIdDetailTransactionsOutDto findById(String id) throws Exception;
+	FindByIdResDetailTransactionsOutDto findById(String id) throws Exception;
 	
 	FindByIdResHeaderDto findByIdHeader(String idHeader) throws Exception;
 }
