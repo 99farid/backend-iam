a6b4aeea55d80ca12d60bd32e466692d3f8a2124
99farid <99.faridazhari@gmail.com>
2021-12-17 17:49:07 +0700

    update dto menyesuaikan findByFilter

---
+++ assets-management/src/main/java/com/lawencon/assetsmanagement/dto/companies/FindAllFilterByCodeResCompaniesDto.java
@@ -0,0 +1,28 @@
+package com.lawencon.assetsmanagement.dto.companies;
+
+import java.util.List;
+
+import com.lawencon.assetsmanagement.model.Companies;
+
+public class FindAllFilterByCodeResCompaniesDto {
+
+	private List<Companies> data;
+	
+	private String msg;
+
+	public List<Companies> getData() {
+		return data;
+	}
+
+	public void setData(List<Companies> data) {
+		this.data = data;
+	}
+
+	public String getMsg() {
+		return msg;
+	}
+
+	public void setMsg(String msg) {
+		this.msg = msg;
+	}
+}
---
+++ assets-management/src/main/java/com/lawencon/assetsmanagement/dto/invoices/FindAllFilterByCodeResInvoicesDto.java
@@ -0,0 +1,23 @@
+package com.lawencon.assetsmanagement.dto.invoices;
+
+import java.util.List;
+
+import com.lawencon.assetsmanagement.model.Invoices;
+
+public class FindAllFilterByCodeResInvoicesDto {
+	private List<Invoices> data;
+	private String msg;
+	public List<Invoices> getData() {
+		return data;
+	}
+	public void setData(List<Invoices> data) {
+		this.data = data;
+	}
+	public String getMsg() {
+		return msg;
+	}
+	public void setMsg(String msg) {
+		this.msg = msg;
+	}
+	
+}
