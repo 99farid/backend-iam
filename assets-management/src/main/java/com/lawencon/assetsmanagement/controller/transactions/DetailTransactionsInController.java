8bdb920f001032f96afa71c2383f27b5d21fc2eb
savitaarsarini <desakayuputu5@gmail.com>
2021-12-21 09:11:05 +0700

    perbaikan nama di detail controller

--- assets-management/src/main/java/com/lawencon/assetsmanagement/controller/transactions/DetailTransactionsInController.java
+++ assets-management/src/main/java/com/lawencon/assetsmanagement/controller/transactions/DetailTransactionsInController.java
@@ -5,12 +5,16 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.lawencon.assetsmanagement.dto.detailtransactionsin.FindAllResDetailTransactionInDto;
 import com.lawencon.assetsmanagement.dto.detailtransactionsin.FindByIdHeaderResDetailTransactionInDto;
 import com.lawencon.assetsmanagement.dto.detailtransactionsin.FindByIdResDetailTransactionInDto;
 import com.lawencon.assetsmanagement.service.DetailTransactionsInService;
 
+@RestController
+@RequestMapping("detail-transactions-in")
 public class DetailTransactionsInController {
 	@Autowired
 	private DetailTransactionsInService detailInService;
