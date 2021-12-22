f119db6722641e315aa3f17aadb5f752fab6a428
savitaarsarini <desakayuputu5@gmail.com>
2021-12-22 18:42:42 +0700

    penambahan model email

---
+++ assets-management/src/main/java/com/lawencon/assetsmanagement/email/EmailModel.java
@@ -0,0 +1,44 @@
+package com.lawencon.assetsmanagement.email;
+
+public class EmailModel {
+
+	private String to;
+	
+	private String subject;
+	
+	private String text;
+	
+	private byte[] fileToAttach;
+
+	public String getTo() {
+		return to;
+	}
+
+	public void setTo(String to) {
+		this.to = to;
+	}
+
+	public String getSubject() {
+		return subject;
+	}
+
+	public void setSubject(String subject) {
+		this.subject = subject;
+	}
+
+	public String getText() {
+		return text;
+	}
+
+	public void setText(String text) {
+		this.text = text;
+	}
+
+	public byte[] getFileToAttach() {
+		return fileToAttach;
+	}
+
+	public void setFileToAttach(byte[] fileToAttach) {
+		this.fileToAttach = fileToAttach;
+	}
+}
