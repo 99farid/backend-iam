24570f1969cbc2e872240bc48d95b4ea8d0d1ee4
savitaarsarini <desakayuputu5@gmail.com>
2021-12-22 18:41:34 +0700

    penambahan send file ke email

--- assets-management/src/main/java/com/lawencon/assetsmanagement/email/EmailHandler.java
+++ assets-management/src/main/java/com/lawencon/assetsmanagement/email/EmailHandler.java
@@ -1,12 +1,18 @@
 package com.lawencon.assetsmanagement.email;
 
+import java.util.Collection;
+import java.util.Map;
+
 import javax.mail.internet.MimeMessage;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.ByteArrayResource;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
+
+import com.lawencon.util.JasperUtil;
 
 @Component
 public class EmailHandler {
@@ -25,7 +31,27 @@ public class EmailHandler {
         helper.setTo(to);
         helper.setSubject(subject);
         helper.setText("<b>"+text+"</b>",true);
+       
         mailSender.send(message);
     }
+    
+    public void sendMailWithAttachmentJasper(EmailModel email, Collection<?> datas, 
+    		String jrxmlName, Map<String, Object> mapParams) throws Exception {
 
+
+        MimeMessage message = mailSender.createMimeMessage();
+
+        MimeMessageHelper helper = new MimeMessageHelper(message, true);
+        
+        ByteArrayResource attachment = new ByteArrayResource(JasperUtil.responseToByteArray(datas, jrxmlName, mapParams));
+
+        helper.setFrom("noreply@baeldung.com");
+        helper.setTo(email.getTo());
+        helper.setSubject(email.getSubject());
+        helper.setText("<b>"+email.getText()+"</b>",true);
+        helper.addAttachment(jrxmlName + ".pdf", attachment);
+        mailSender.send(message);
+    	
+   }
+    
 }
