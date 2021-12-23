c5a4b096fd6adedd1e1bd35e27eccf2d57645129
99farid <99.faridazhari@gmail.com>
2021-12-23 09:27:23 +0700

    membuat scheduler pengirim email

---
+++ assets-management/src/main/java/com/lawencon/assetsmanagement/scheduler/TrasactionDetailSchedule.java
@@ -0,0 +1,19 @@
+package com.lawencon.assetsmanagement.scheduler;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import com.lawencon.assetsmanagement.service.DetailTransactionsOutService;
+
+@Component
+public class TrasactionDetailSchedule {
+	
+	@Autowired
+	private DetailTransactionsOutService detailService;
+	
+	@Scheduled(fixedDelay = 600000)
+	public void dueDateReminder() throws Exception{
+		detailService.sendReportDueDate();
+	}
+}
