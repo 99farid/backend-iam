7a6e260f1e6030a4de027e56e6061d84fdd1f002
savitaarsarini <desakayuputu5@gmail.com>
2021-12-28 18:14:31 +0700

    penambahan service rolepermission

--- assets-management/src/main/java/com/lawencon/assetsmanagement/service/RolePermissionsService.java
+++ assets-management/src/main/java/com/lawencon/assetsmanagement/service/RolePermissionsService.java
@@ -1,8 +1,12 @@
 package com.lawencon.assetsmanagement.service;
 
+import java.util.List;
+
 import com.lawencon.assetsmanagement.dto.DeleteResDataDto;
+import com.lawencon.assetsmanagement.dto.rolepermissions.FindAllResFilterByRoleDto;
 import com.lawencon.assetsmanagement.dto.rolepermissions.FindAllResRolePermissionsDto;
 import com.lawencon.assetsmanagement.dto.rolepermissions.FindByIdResRolePermissionsDto;
+import com.lawencon.assetsmanagement.model.RolePermissions;
 
 public interface RolePermissionsService {
 
@@ -13,4 +17,6 @@ public interface RolePermissionsService {
 //	UpdateResDto update(RolePermissions data) throws Exception;
 	
 	DeleteResDataDto removeById(String id) throws Exception;
+	
+	FindAllResFilterByRoleDto findAllFilterByRole(String idRole) throws Exception;
 }
