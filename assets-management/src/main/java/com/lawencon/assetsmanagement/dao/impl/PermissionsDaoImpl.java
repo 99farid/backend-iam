0957f1acb4e33b9f92153657507e9aaf96938ec0
savitaarsarini <desakayuputu5@gmail.com>
2021-12-28 18:13:39 +0700

    perbaikan try catch dao impl permissions

--- assets-management/src/main/java/com/lawencon/assetsmanagement/dao/impl/PermissionsDaoImpl.java
+++ assets-management/src/main/java/com/lawencon/assetsmanagement/dao/impl/PermissionsDaoImpl.java
@@ -4,6 +4,8 @@ import java.sql.Timestamp;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.persistence.NoResultException;
+
 import org.springframework.stereotype.Repository;
 
 import com.lawencon.assetsmanagement.dao.PermissionsDao;
@@ -32,7 +34,9 @@ public class PermissionsDaoImpl extends BaseDaoImpl<Permissions> implements Perm
 		queryBuilder.append("WHERE p.permission_name LIKE %%:input%%");
 
 		String sql = queryBuilder.toString();
-		List<?> result = createNativeQuery(sql).setParameter("input", input).getResultList();
+		List<?> result = createNativeQuery(sql)
+				.setParameter("input", input)
+				.getResultList();
 
 		List<Permissions> resultListPermissions = new ArrayList<>();
 		result.forEach(rs -> {
@@ -72,35 +76,42 @@ public class PermissionsDaoImpl extends BaseDaoImpl<Permissions> implements Perm
 
 	@Override
 	public Permissions findByCode(String code) throws Exception {
-		StringBuilder queryBuilder = new StringBuilder();
-		queryBuilder.append("SELECT id, code, permission_name, ");
-		queryBuilder.append("ver, created_by, created_date, updated_by, updated_date, is_active ");
-		queryBuilder.append("FROM permissions AS p ");
-		queryBuilder.append("WHERE code = :code");
+		try {
+			StringBuilder queryBuilder = new StringBuilder();
+			queryBuilder.append("SELECT id, code, permission_name, ");
+			queryBuilder.append("ver, created_by, created_date, updated_by, updated_date, is_active ");
+			queryBuilder.append("FROM permissions AS p ");
+			queryBuilder.append("WHERE code = :code");
 
-		String sql = queryBuilder.toString();
-		Object result = createNativeQuery(sql).setParameter("code", code).getSingleResult();
+			String sql = queryBuilder.toString();
+			Object result = createNativeQuery(sql)
+					.setParameter("code", code)
+					.getSingleResult();
 
-		Permissions permission = null;
+			Permissions permission = null;
 
-		if (result != null) {
-			Object[] objArr = (Object[]) result;
+			if (result != null) {
+				Object[] objArr = (Object[]) result;
 
-			permission = new Permissions();
-			permission.setId(objArr[0].toString());
-			permission.setCode(objArr[1].toString());
-			permission.setPermissionName(objArr[2].toString());
-			permission.setVersion(Long.valueOf(objArr[3].toString()));
-			permission.setCreatedBy(objArr[4].toString());
-			permission.setCreatedDate(Timestamp.valueOf(objArr[5].toString()).toLocalDateTime());
-			if (objArr[6] != null) {
-				permission.setUpdatedBy(objArr[6].toString());
+				permission = new Permissions();
+				permission.setId(objArr[0].toString());
+				permission.setCode(objArr[1].toString());
+				permission.setPermissionName(objArr[2].toString());
+				permission.setVersion(Long.valueOf(objArr[3].toString()));
+				permission.setCreatedBy(objArr[4].toString());
+				permission.setCreatedDate(Timestamp.valueOf(objArr[5].toString()).toLocalDateTime());
+				if (objArr[6] != null) {
+					permission.setUpdatedBy(objArr[6].toString());
+				}
+				if (objArr[7] != null) {
+					permission.setUpdatedDate(Timestamp.valueOf(objArr[7].toString()).toLocalDateTime());
+				}
+				permission.setIsActive(Boolean.valueOf(objArr[8].toString()));
 			}
-			if (objArr[7] != null) {
-				permission.setUpdatedDate(Timestamp.valueOf(objArr[7].toString()).toLocalDateTime());
-			}
-			permission.setIsActive(Boolean.valueOf(objArr[8].toString()));
+			return permission;
+		} catch (NoResultException e) {
+			e.printStackTrace();
+			return null;
 		}
-		return permission;
 	}
 }
