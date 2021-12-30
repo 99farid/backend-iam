5f14a43f290deb597755673919798f0c93b56978
savitaarsarini <desakayuputu5@gmail.com>
2021-12-30 18:04:41 +0700

    perbaikan dto asset dan location

---
+++ assets-management/src/main/java/com/lawencon/assetsmanagement/dto/assets/FindAllFilterBySearchResComponentDto.java
@@ -0,0 +1,28 @@
+package com.lawencon.assetsmanagement.dto.assets;
+
+import java.util.List;
+
+import com.lawencon.assetsmanagement.model.Assets;
+
+public class FindAllFilterBySearchResComponentDto {
+
+	private List<Assets> data;
+	
+	private String msg;
+
+	public List<Assets> getData() {
+		return data;
+	}
+
+	public void setData(List<Assets> data) {
+		this.data = data;
+	}
+
+	public String getMsg() {
+		return msg;
+	}
+
+	public void setMsg(String msg) {
+		this.msg = msg;
+	}
+}
---
+++ assets-management/src/main/java/com/lawencon/assetsmanagement/dto/locations/FindAllFilterBySearchResLocationsDto.java
@@ -0,0 +1,28 @@
+package com.lawencon.assetsmanagement.dto.locations;
+
+import java.util.List;
+
+import com.lawencon.assetsmanagement.model.Locations;
+
+public class FindAllFilterBySearchResLocationsDto {
+
+	private List<Locations> data;
+	
+	private String msg;
+
+	public List<Locations> getData() {
+		return data;
+	}
+
+	public void setData(List<Locations> data) {
+		this.data = data;
+	}
+
+	public String getMsg() {
+		return msg;
+	}
+
+	public void setMsg(String msg) {
+		this.msg = msg;
+	}
+}
