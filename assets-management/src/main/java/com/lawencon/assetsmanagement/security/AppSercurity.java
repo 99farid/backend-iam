c33f0b2c89a90a30f582dc7532bfff3f3466b19d
savitaarsarini <desakayuputu5@gmail.com>
2022-01-05 16:34:46 +0700

    appsecurity di backend savita

--- assets-management/src/main/java/com/lawencon/assetsmanagement/security/AppSercurity.java
+++ assets-management/src/main/java/com/lawencon/assetsmanagement/security/AppSercurity.java
@@ -1,6 +1,7 @@
 package com.lawencon.assetsmanagement.security;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Profile;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -43,6 +44,7 @@ public class AppSercurity extends WebSecurityConfigurerAdapter{
 	@Override
 	public void configure(WebSecurity web) throws Exception {
 		web.ignoring()
+		.antMatchers(HttpMethod.POST, "/users")		
 		.antMatchers(HttpMethod.GET, "/swagger-ui.html", "/swagger-ui/**", "/v3/api-docs/**", "/assets/pic/**", "/track-activities/**", "/companies/search/**");
 		
 	}
